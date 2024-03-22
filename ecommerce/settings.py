8d39790cd7f16793f483a3a360f87b25f66d13a3
dvooskid <dvooskid@localhost.localdomain>
2024-03-22 17:44:46 +0100

    Finishing Build

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "store.apps.StoreConfig",
-    "embed_video",
 ]
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -73,19 +73,19 @@ WSGI_APPLICATION = "ecommerce.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "verceldb",
-#         "USER": "default",
-#         "PASSWORD": "fVBsW4J1njyZ",
-#         "HOST": "ep-muddy-mud-a2ukbu39.eu-central-1.postgres.vercel-storage.com",
-#         "PORT": "5432",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "verceldb",
+        "USER": "default",
+        "PASSWORD": "fVBsW4J1njyZ",
+        "HOST": "ep-muddy-mud-a2ukbu39.eu-central-1.postgres.vercel-storage.com",
+        "PORT": "5432",
+    }
+}
 
-DATABASES = {"default": {
-    "ENGINE": "django.db.backends.sqlite3", "NAME": "db.sqlite3"}}
+# DATABASES = {"default": {
+#     "ENGINE": "django.db.backends.sqlite3", "NAME": "db.sqlite3"}}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
